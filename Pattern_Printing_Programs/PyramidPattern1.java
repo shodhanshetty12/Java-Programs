b3d513738ae5cd7c6bb80ba818a5b5ebaa3b9c41
Shodhan kumar shetty <163324594+shodhanshetty12@users.noreply.github.com>
2025-07-16 21:45:47 +0530

    Create PyramidPattern1.java

---
+++ Pattern_Printing_Programs/PyramidPattern1.java
@@ -0,0 +1,12 @@
+// Simple pyramid of stars
+public class PyramidPattern1 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+            }
+        }
+    }
+}
